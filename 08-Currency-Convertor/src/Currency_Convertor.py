ffa544e84caee5be2ac147c3b929fd6e38fe65c8
Soheila Abbasi Habashi <soheila.abbasi7@gmail.com>
2026-09-24 23:55:02 +0200

    Add Currency_Convertor.py

--- 08-Currency-Convertor/src/Currency_Convertor.py
+++ 08-Currency-Convertor/src/Currency_Convertor.py
@@ -4,6 +4,8 @@ import requests
 def get_exchange_rate(base_currency, target_currency):
     url = f"https://api.exchangerate-api.com/v4/latest/{base_currency}"
     response = requests.get(url)
+    if response.status_code != 200:
+        return None
     return response.json()['rates'][target_currency]
 
 
@@ -18,3 +20,5 @@ if __name__ == '__main__':
     exchange_rate = get_exchange_rate(base_currency, target_currency)
     converted_amount = convert_currency(amount, exchange_rate)
     print(f"{amount} {base_currency} is {converted_amount} {target_currency}")
+
+    
